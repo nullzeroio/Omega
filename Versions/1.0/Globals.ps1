37334e8e8c49d03b681aa91deb79a94c25d77b3e
Kevin Kirkpatrick <kevin@nullzero.io>
2018-05-21 00:04:16 -0400

    Initial pump
    Signed-off-by: Kevin Kirkpatrick <kevin@nullzero.io>

---
+++ Versions/1.0/Globals.ps1
@@ -0,0 +1,40 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+<#
+Omega - Community Edition (Omega-CE)
+    Copyright (C) 2013  Kevin Kirkpatrick
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#>
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{ 
+	if($hostinvocation -ne $null)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
